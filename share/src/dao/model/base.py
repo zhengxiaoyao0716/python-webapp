c61a63a3fbd9ce6b0f0919d6cef534ab2b864b40
zhengxiaoyao0716 <1499383852@qq.com>
2016-11-05 13:44:32 +0800

    完善base model

--- share/src/dao/model/base.py
+++ share/src/dao/model/base.py
@@ -3,25 +3,36 @@
 数据库基础模板
 """
 
-from sqlalchemy.ext.declarative import declarative_base as _base
+from datetime import timedelta, datetime
 
-from sqlalchemy import Column, Integer, String, Text, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base as _base
+from sqlalchemy import Column, Integer, String, Text, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 
 from dao.connect import db_session
 
 
+def get_time(delta_day=0, delta_second=0):
+    """获取时间"""
+    return str(datetime.now() + timedelta(delta_day, delta_second))[0:19]
+
+
 Base = _base()
 Base.query = db_session.query_property()
 
-def _column_dict(self):
-    """直接读取列字典"""
+
+def simple_model_dict(self):
+    """获取实例字典"""
     model_dict = dict(self.__dict__)
     del model_dict['_sa_instance_state']
     return model_dict
-Base.column_dict = _column_dict
-def _add_new(cls, *args, **kwargs):
+Base.simple = simple_model_dict
+Base.__repr__ = lambda self: str(simple_model_dict(self))
+
+
+def append_model_instance(cls, *args, **kwargs):
+    """添加新的实例"""
     self = cls(*args, **kwargs)
     db_session.add(self)
     return self
-Base.add_new = classmethod(_add_new)
+Base.append = classmethod(append_model_instance)
