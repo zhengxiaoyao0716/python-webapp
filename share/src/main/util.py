803b5d33af870b82399cce911a0dfc99a6c7988a
zhengxiaoyao0716 <1499383852@qq.com>
2016-11-06 15:03:55 +0800

    修改创建响应son的逻辑，then参数不好抽象

--- share/src/main/util.py
+++ share/src/main/util.py
@@ -7,23 +7,15 @@
 from flask import json, url_for
 
 
-def make_resp(body, then=None, then_param=None):
-    """创建响应"""
-    then_param = then_param or {}
-    return json.dumps(
-        {
-            'flag': True, 'body': body,
-            'then': then and url_for(then, **then_param)
-        }
-    ), 200, {'Content-Type': 'application/json'}
+def make_resp(body, then=None):
+    """创建响应(json)"""
+    return json.dumps({
+        'flag': True, 'body': body, 'then': then
+    }), 200, {'Content-Type': 'application/json'}
 
 
-def make_err(reas, then=None, status=403, then_param=None):
-    """创建响应"""
-    then_param = then_param or {}
-    return json.dumps(
-        {
-            'flag': False, 'reas': reas,
-            'then': then and url_for(then, **then_param)
-        }
-    ), status, {'Content-Type': 'application/json'}
+def make_err(reas, then=None, status=403):
+    """创建响应(json)"""
+    return json.dumps({
+        'flag': False, 'reas': reas, 'then': then
+    }), status, {'Content-Type': 'application/json'}
